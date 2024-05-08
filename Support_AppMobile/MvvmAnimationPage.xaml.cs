fc225d9e5700bc56767afb07aa50ae0c8880c5ba
Nima <pu78ifh@eduvaud.ch>
2024-05-08 17:18:14 +0200

    Animation completete, animation MVVM aussi complete

---
+++ Support_AppMobile/MvvmAnimationPage.xaml.cs
@@ -0,0 +1,36 @@
+using Support_AppMobile.ViewModels;
+
+namespace Support_AppMobile
+{
+    public partial class MvvmAnimationPage : ContentPage
+    {
+        public MvvmAnimationPage()
+        {
+            InitializeComponent();
+
+            var vm = BindingContext as MvvmAnimationPageViewModel;
+
+            // Ecriture standard avec une methode liée
+            vm.RotateBoxUIAction = RotateUI;
+
+            vm.MoveBoxUIAction = TranslateUI;
+
+            vm.CancelBoxAnimationUIAction = CancelAnimationMvvmUI;
+
+        }
+        private void CancelAnimationMvvmUI()
+        {
+            this.MvvmAnimationPageImage.CancelAnimations();
+        }
+
+        private void RotateUI(int angle)
+        {
+            this.MvvmAnimationPageImage.RotateTo(angle);
+        }
+
+        private void TranslateUI(int multiplier)
+        {
+            this.MvvmAnimationPageImage.TranslationX -= multiplier;
+        }
+    }
+}
---
+++ Support_AppMobile/ViewModels/MvvmAnimationPageViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Support_AppMobile.ViewModels
+{
+    public partial class MvvmAnimationPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool toggled = false;
+
+        [ObservableProperty]
+        private string label = "Rotate 90°";
+
+        [ObservableProperty]
+        private int speed = 1;
+
+        public Action<int>? RotateBoxUIAction { private get; set; }
+
+        private void RotateBox(int angle)
+        {
+            if (RotateBoxUIAction != null)
+            {
+                RotateBoxUIAction.Invoke(angle);
+            }
+            else
+            {
+                Trace.WriteLine($"No rotation action defined, would rotate {angle}");
+            }
+        }
+
+        public Action<int>? MoveBoxUIAction { private get; set; }
+
+        [RelayCommand]
+        private void MoveBox(int multiplier)
+        {
+            if (MoveBoxUIAction != null)
+            {
+                MoveBoxUIAction.Invoke(Speed*multiplier);
+            }
+            else
+            {
+                Trace.WriteLine($"No move action defined, would move x {Speed*multiplier}");
+            }
+        }
+
+        public Action? CancelBoxAnimationUIAction { private get; set; }
+
+        [RelayCommand]
+        private void CancelBoxAnimation()
+        {
+            if (CancelBoxAnimationUIAction != null)
+            {
+                CancelBoxAnimationUIAction.Invoke();
+            }
+        }
+
+        partial void OnToggledChanged(bool value)
+        {
+            RotateBox(value ? 90 : 0);
+            Label = value ? "Rotate back" : "Rotate 90°";
+        }
+    }
+
+
+}
